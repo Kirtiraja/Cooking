82be3f68c42d3b56af11efa7a7c70129e258d77c
Student <example@example.com>
2019-10-21 13:35:04 -0700

    add cups convert to liters function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,13 @@ var liters = function(gals) {
 var gal = parseFloat(prompt("Enter gallons:"));
 var result = liters(gal);
  alert(result);
- 
+
+
+
+ var liters = function(cups) {
+   return cups / 4.227;
+ };
+
+ var cups = parseFloat(prompt("Enter cups:"));
+ var result = liters(cups);
+  alert(result);
